7ccde830a7c0478ae6b5259d705bf12257033aae
nourhene <benothmennourhen8@gmail.com>
2025-06-30 19:57:19 +0100

    ✨ Ajout des contrôleurs de mise à jour et suppression d'utilisateur

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,71 @@ const getUserController = async (req, res) => {
   }
 };
 
+// Contrôleur pour mettre à jour un utilisateur
+const UpdateUserController = async (req, res) => {
+  try {
+    const userId = req.params.id; // Récupère l'id depuis les paramètres de l'URL
+    const updateData = req.body;  // Récupère les données depuis le body
+
+    const updatedUser = await userModel.findByIdAndUpdate(
+      userId,
+      updateData,
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      user: updatedUser
+    });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Error on update user API"
+    });
+  }
+};
+// Supprimer un utilisateur
+const deleteUserController = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const deletedUser = await userModel.findByIdAndDelete(userId);
+
+    if (!deletedUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "User deleted successfully"
+    });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Error while deleting user"
+    });
+  }
+};
+
+
 // Export
 module.exports = {
-  getUserController
+  getUserController,
+  UpdateUserController,
+  deleteUserController
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { getUserController } = require('../controllers/userController');
+const { getUserController, UpdateUserController, deleteUserController } = require('../controllers/userController');
 const requireSignIn = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 // Route protégée nécessitant une authentification
-router.get('/user',requireSignIn, getUserController);
+router.get('/users', getUserController);
+
+// Mettre à jour un utilisateur
+router.put('/users/:id', UpdateUserController);
+
+// Supprimer un utilisateur
+router.delete('/users/:id',deleteUserController);
 
 module.exports = router;
